4b0f8c7259873657cd1a473f0b58437bbab45a41
Guudz <122683344+Guudz@users.noreply.github.com>
2023-01-14 17:19:18 +0100

    Initial commit

---
+++ ESX HOLDUP/esx_blanchisseur/config.lua
@@ -0,0 +1,15 @@
+Config					= {}
+Config.Locale			= 'fr'
+Config.Slice			= 1000
+Config.Percentage		= 90  -- pourcentage d'argent que le blanchisseur nous donne sur 100$ d'argent sale, il nous donne 90$ avec cette configuration.
+Config.MarkerSize   	= {x = 3.0, y = 3.0, z = 3.0}
+Config.openHours		= 0
+Config.closeHours		= 5
+Config.Blip 			= false
+Config.Hours 			= false
+Config.Menu				= true 
+Config.Bonus			= {min = 0, max = 0}
+
+Config.Zones = {
+	{x = 786.8830, y = -543.0357, z = 22.47620,percent=30},
+}
